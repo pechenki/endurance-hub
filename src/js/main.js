5199507382c128d031721330d720df4e2324e410
pechenki <lename.lenaya@gmail.com>
2024-11-12 11:14:50 +0200

    added rough markup

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,63 @@
+$(document).ready(function(){
+    $('.js-choice-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev choice-slider-arrow choice-slider-arrow--prev">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-chevron-left" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>\n' +
+            '</svg>' +
+            '</button>',
+        nextArrow: '<button type="button" class="slick-next choice-slider-arrow choice-slider-arrow--next">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-chevron-right" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>\n' +
+            '</svg>' +
+            '</button>',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
 
-console.log("JavaScript is working!");
+    $('.js-team-slider').slick({
+        centerMode: true,
+        centerPadding: '60px',
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev choice-slider-arrow choice-slider-arrow--prev">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-chevron-left" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>\n' +
+            '</svg>' +
+            '</button>',
+        nextArrow: '<button type="button" class="slick-next choice-slider-arrow choice-slider-arrow--next">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-chevron-right" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>\n' +
+            '</svg>' +
+            '</button>',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    arrows: false,
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 const isDevelopment = process.env.NODE_ENV !== "production";
 
 module.exports = {
-    entry: ["./src/js/main.js", "./src/scss/styles.scss"],  // Include styles.scss as an entry point for dev
+    entry: ["./src/js/main.js", "./src/scss/styles.scss"],
 
     output: {
         path: path.resolve(__dirname, "dist"),
-        filename: "bundle.js",  // Output JS file
-        clean: true,  // Clean the dist folder before every build
+        filename: "bundle.js",
+        clean: true, // Clean dist folder before every build
+        // assetModuleFilename: "assets/[name][ext][query]", // Path for images and other assets
     },
 
     module: {
@@ -19,39 +21,70 @@ module.exports = {
             {
                 test: /\.scss$/,
                 use: [
-                    isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader, // Inject styles in dev, extract in prod
-                    "css-loader",  // CSS loader
-                    "sass-loader",  // Sass loader
+                    isDevelopment ? "style-loader" : MiniCssExtractPlugin.loader,
+                    "css-loader",
+                    "sass-loader",
                 ],
             },
             // HTML Loader
             {
                 test: /\.html$/,
-                use: ["html-loader"],  // Handle HTML imports
+                use: ["html-loader"],
+            },
+            // Image Loader (for dynamic image processing)
+            {
+                test: /\.(png|jpe?g|gif|svg)$/i,
+                type: 'asset/resource', // Use Webpack's asset module
+                generator: {
+                    filename: 'images/[name].[hash:8][ext]', // Path and name for output images
+                },
+            },
+            {
+                test: /\.(woff|woff2|ttf)$/i,
+                type: 'asset/resource', // Use Webpack's asset module
+                generator: {
+                    filename: 'fonts/[name].[hash:8][ext]', // Path and name for output fonts
+                },
             },
         ],
     },
 
     plugins: [
-        // HTML Webpack Plugin
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",  // Your HTML template
-            filename: "index.html",  // Output HTML file
+        new CopyWebpackPlugin({
+            patterns: [
+                {
+                    from: path.resolve(__dirname, 'src/images'),  // from src/images folder
+                    to: path.resolve(__dirname, 'dist/images'),    // to dist/images folder
+                },
+                {
+                    from: path.resolve(__dirname, 'src/fonts'),  // from src/images folder
+                    to: path.resolve(__dirname, 'dist/fonts'),    // to dist/images folder
+                },
+            ],
         }),
-
-        // CSS Extract Plugin (Only in production)
-        !isDevelopment && new MiniCssExtractPlugin({ filename: "styles.css" }),  // Extract styles in production
-    ].filter(Boolean),  // Remove MiniCssExtractPlugin in dev mode to avoid extracting CSS
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+            filename: "index.html",
+            inject: true,
+        }),
+        !isDevelopment && new MiniCssExtractPlugin({ filename: "styles.css" }),
+    ].filter(Boolean),
 
     devServer: {
         static: {
-            directory: path.join(__dirname, "dist"),  // Serve static files from the dist directory
+            directory: path.join(__dirname, "dist"),
         },
         compress: true,
         port: 9000,
-        open: true,  // Open the browser automatically
-        hot: true,   // Enable Hot Module Replacement (HMR)
+        open: true,
+        hot: true, // Enable Hot Module Replacement (HMR)
+        watchFiles: {
+            paths: ["src/images/**/*", "src/scss/**/*", "src/**/*.html"], // Watch changes to images
+        },
+        devMiddleware: {
+            writeToDisk: true, // Force images and other assets to be written to disk
+        },
     },
 
-    mode: isDevelopment ? "development" : "production",  // Set mode based on NODE_ENV
+    mode: isDevelopment ? "development" : "production",
 };

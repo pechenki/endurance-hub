8f4c01456a00c67ee5f7858e89243a22fb2f940c
Olena Velykotska <olena.velykotska@planeks.net>
2025-02-03 09:10:27 +0200

    made content changes

--- src/js/main.js
+++ src/js/main.js
@@ -65,6 +65,34 @@ $(document).ready(function(){
         ]
     });
 
+    $('.js-partners-slider').not('.slick-initialized').slick({
+        infinite: true,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        prevArrow: '<button type="button" class="slick-prev choice-slider-arrow choice-slider-arrow--prev">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-chevron-left" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0"/>\n' +
+            '</svg>' +
+            '</button>',
+        nextArrow: '<button type="button" class="slick-next choice-slider-arrow choice-slider-arrow--next">' +
+            '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="bi bi-chevron-right" viewBox="0 0 16 16">\n' +
+            '  <path fill-rule="evenodd" d="M4.646 1.646a.5.5 0 0 1 .708 0l6 6a.5.5 0 0 1 0 .708l-6 6a.5.5 0 0 1-.708-.708L10.293 8 4.646 2.354a.5.5 0 0 1 0-.708"/>\n' +
+            '</svg>' +
+            '</button>',
+        responsive: [
+            {
+                breakpoint: 480,
+                settings: {
+                    centerMode: true,
+                    centerPadding: '40px',
+                    slidesToShow: 1
+                }
+            }
+        ]
+    });
+
     $('.js-validate').on('submit', function (e) {
         console.log('SUBMIT')
         e.preventDefault();
